980db5c91cedc11f43d796f0dc3d6969eb7d111d
theisub <theisub1@gmail.com>
2019-09-12 01:49:38 +0300

    Changed values to check if Azure reacts

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace DSIP_.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "value123", "value231" };
         }
 
         // GET api/values/5
